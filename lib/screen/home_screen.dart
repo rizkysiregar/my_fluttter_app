90e6a882a050d9e68d837bf16007ba15b5062c04
rizde <regarrezky@gmail.com>
2023-07-05 22:27:36 +0700

    add payment status bar in home

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,14 +12,14 @@ class HomeScreen extends StatelessWidget {
           decoration: BoxDecoration(
               color: Colors.white, borderRadius: BorderRadius.circular(10)),
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          width: 200,
-          height: 40,
+          width: 350,
+          height: 43,
           child: const TextField(
-            decoration: InputDecoration(
-                hintText: "Cari Produk disini!",
-                icon: Icon(Icons.search),
-                border: InputBorder.none),
-          ),
+              decoration: InputDecoration(
+                  hintText: "Cari Produk disini!",
+                  icon: Icon(Icons.search),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 8))),
         ),
         actions: <Widget>[
           IconButton(
@@ -40,6 +40,61 @@ class HomeScreen extends StatelessWidget {
                 Icons.shopping_cart_outlined,
                 color: Colors.white,
               ))
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 25,
+          ),
+          Card(
+            margin: const EdgeInsets.only(left: 16, right: 16),
+            elevation: 4,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            child: Container(
+              // container for payment status
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+
+              height: 65,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(4)),
+              child: const Center(
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.payments_rounded),
+                    SizedBox(width: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[Text('Rp0'), Text('Saldo anda')],
+                    ),
+                    SizedBox(width: 75),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.arrow_upward_rounded,
+                          color: Colors.lightBlue,
+                        ),
+                        Text('Top Up')
+                      ],
+                    ),
+                    SizedBox(width: 30),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.history,
+                          color: Colors.lightBlue,
+                        ),
+                        Text('Top Up')
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
